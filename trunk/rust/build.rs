123fd3b5d37413a67c7398ce0d0cedd6a8fc4a4c
correy <112426112@qq.com>
2023-06-27 19:11:31 +0800

    添加rust的使用代码。

---
+++ trunk/rust/build.rs
@@ -0,0 +1,39 @@
+use std::env;
+use std::path::PathBuf;
+
+fn main() {
+    // Configure and generate bindings.
+    let bindings = bindgen::builder()
+        .use_core()
+
+        //.blocklist_type("embedded_panic_header") //https://github.com/MozhuCY/osx-sys/commit/4f7265b6757499355696e4e5d1a991ee9a26dbfb
+        //.blocklist_type("embedded_panic_header__bindgen_ty_1")
+
+        .blocklist_type("IMAGE_TLS_DIRECTORY") //https://github.com/Rust-SDL2/rust-sdl2/issues/1288
+        .blocklist_type("PIMAGE_TLS_DIRECTORY")
+        .blocklist_type("IMAGE_TLS_DIRECTORY64")
+        .blocklist_type("PIMAGE_TLS_DIRECTORY64")
+        .blocklist_type("_IMAGE_TLS_DIRECTORY64")
+
+        .header("../inc/Process.h")
+        //.clang_arg("-x").clang_arg("c++")
+        .generate()
+        .expect("Couldn't write bindings!");
+
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+
+    // Write the generated bindings to an output file.
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))//"../inc/libnet.rs" 好家伙生成的文件达20多MB，66万行之多，运行也得几分钟。
+        .expect("Couldn't write bindings!");
+
+        //相当于：#[link(name = "..\\x64\\Debug\\Process")]，此办法经测试有效。
+        println!("cargo:rustc-link-search=native={}", "..\\x64\\Debug");
+        println!("cargo:rustc-link-lib=dylib=Process");
+        println!("cargo:rerun-if-changed=../inc/Process.h");
+
+        // cxx_build::bridge("src/main.rs")  // returns a cc::Build
+        // .file("../inc/libnet.h")
+        // .flag_if_supported("-std=c++11")
+        // .compile("cxxbridge-demo");
+}
---
+++ trunk/rust/src/main.rs
@@ -0,0 +1,28 @@
+/*
+如果由c转的rs文件在编译是出现：error[E0588]: packed type cannot transitively contain a `#[repr(align)]` type
+直接注释掉#[repr(align)]这一行即可，但是相关的测试可能错误：内存对齐问题。
+另一个思路是改为：#[repr(align(1))]
+*/
+
+// Suppress the flurry of warnings caused by using "C" naming conventions
+#![allow(non_upper_case_globals)]
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
+
+//应该有yml的配置方法。
+//bindgen对位的处理还不太好。
+// #[link(name = "..\\x64\\Debug\\libnet")]
+// extern "system"
+// {
+// 	//可以为空。
+// }
+
+include!(concat!(env!("OUT_DIR"), "\\bindings.rs"));
+
+fn main() {//测试的时候，要把dll复制到exe的目录下。
+    println!("Hello, world!");
+
+    unsafe{
+        EnumeratingAllProcesses();
+    }
+}
