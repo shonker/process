8eb1dbe3058036505024d8bfed237586b92dec9a
correy <112426112@qq.com>
2023-08-07 14:54:02 +0800

    test

--- trunk/test/Performance.cpp
+++ trunk/test/Performance.cpp
@@ -2,6 +2,10 @@
 
 
 void PerformanceTest()
+/*
+
+这里测试的，包括注释掉的，都是成功的代码。
+*/
 {
     //CollectPerformanceData(L"\\Processor(*)\\% Processor Time", PDH_FMT_DOUBLE);
     //CollectPerformanceData(L"\\Processor(*)\\% User Time", PDH_FMT_DOUBLE);
@@ -13,12 +17,12 @@ void PerformanceTest()
     //CollectPerformanceData(L"\\PhysicalDisk(*)\\% Disk Time", PDH_FMT_DOUBLE);
     //CollectPerformanceData(L"\\PhysicalDisk(*)\\% Idle Time", PDH_FMT_DOUBLE);
 
-    //CollectPerformanceDatas(L"\\Process(*)\\% Processor Time", PDH_FMT_DOUBLE);
+    CollectPerformanceDatas(L"\\Process(*)\\% Processor Time", PDH_FMT_DOUBLE);
     //CollectPerformanceDatas(L"\\Process(notepad)\\% Processor Time", PDH_FMT_DOUBLE);
     //CollectPerformanceDatas(L"\\Process(svchost#1)\\% Processor Time", PDH_FMT_DOUBLE);//是索引不是PID，从1开始。
     //CollectPerformanceDatas(L"\\Process(*)\\Working Set", PDH_FMT_LARGE);
     //CollectPerformanceDatas(L"\\Process(*)\\ID Process", PDH_FMT_LONG);//类似于PS命令：(Get-Counter "\Process(notepad*)\ID Process").CounterSamples
-    CollectPerformanceDatas(L"\\Process(svchost#1)\\ID Process", PDH_FMT_LONG);
+    //CollectPerformanceDatas(L"\\Process(svchost#1)\\ID Process", PDH_FMT_LONG);
 
     //EnumeratingProcessObjects();
     //EnumObjectItems(L"Process");
--- trunk/test/test.cpp
+++ trunk/test/test.cpp
@@ -48,7 +48,7 @@ int _cdecl main(_In_ int argc, _In_reads_(argc) CHAR * argv[])
     //GetAdaptersAddressesInfo(argc, argv);
     
     //test_c();
-    PerformanceTest();
+    TestGlobalMemoryStatusEx();
 
     LocalFree(Arglist);
 }
