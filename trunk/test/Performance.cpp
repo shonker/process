6c3b2dc4cd97369d16d6dd3864ce9ec4ba0d0818
correy <112426112@qq.com>
2023-08-08 15:54:46 +0800

    测试

--- trunk/test/Performance.cpp
+++ trunk/test/Performance.cpp
@@ -17,9 +17,10 @@ void PerformanceTest()
     //CollectPerformanceData(L"\\PhysicalDisk(*)\\% Disk Time", PDH_FMT_DOUBLE);
     //CollectPerformanceData(L"\\PhysicalDisk(*)\\% Idle Time", PDH_FMT_DOUBLE);
 
-    CollectPerformanceDatas(L"\\Process(*)\\% Processor Time", PDH_FMT_DOUBLE);
+    //CollectPerformanceDatas(L"\\Process(*)\\% Processor Time", PDH_FMT_DOUBLE);
     //CollectPerformanceDatas(L"\\Process(notepad)\\% Processor Time", PDH_FMT_DOUBLE);
     //CollectPerformanceDatas(L"\\Process(svchost#1)\\% Processor Time", PDH_FMT_DOUBLE);//是索引不是PID，从1开始。
+    CollectPerformanceDatas(L"\\Process(procexp_2648)\\% Processor Time", PDH_FMT_DOUBLE);
     //CollectPerformanceDatas(L"\\Process(*)\\Working Set", PDH_FMT_LARGE);
     //CollectPerformanceDatas(L"\\Process(*)\\ID Process", PDH_FMT_LONG);//类似于PS命令：(Get-Counter "\Process(notepad*)\ID Process").CounterSamples
     //CollectPerformanceDatas(L"\\Process(svchost#1)\\ID Process", PDH_FMT_LONG);
